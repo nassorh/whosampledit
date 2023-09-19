2b816ff4e243f53c824c7e4abc9912d4b016ff89
nassorh <nassorh.dev@gmail.com@ --global>
2023-09-19 17:27:35 +0100

    feat: Create Search Component
    Implemented the Search component, allowing users to search for songs.
    
    - Utilised React Bootstrap Form for the search input.
    - Used the useDebounce hook to debounce search queries.
    - Incorporated state management for search query, songs, and loading status.
    - Implemented useEffect to trigger song search based on debounced query changes.
    - Rendered loading status and search results dynamically.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Search from './Compontents/Search';
 
 function App() {
   return (
     <div className="App">
-      
+      <Search />
     </div>
   );
 }
---
+++ src/Compontents/Search.tsx
@@ -0,0 +1,59 @@
+import Form from 'react-bootstrap/Form';
+import { useEffect, useState } from 'react';
+import searchService from '../dependencies'
+import useDebounce from '../Hooks/useDebounce'
+import Song from '../Entities/Song'
+
+function Search() {
+    const [search, setSearch] = useState<string>("")
+    const [songs, setSongs] = useState<typeof Song[] | null>([])    
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const debouncedSearchValue = useDebounce(search)
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        searchService
+            .performSearch(search)
+            .then((response) => {
+                setIsLoading(false)
+                setSongs(response)
+            })
+            .catch((error) => {
+                console.log("Caught",error)
+                setIsLoading(false)
+            });
+    }, [debouncedSearchValue]);
+
+    let content;
+    if(isLoading){
+        content = <p>Loading...</p>
+    }else{
+        content = songs?.map((song,index) => (
+            <div key={index}>
+                <h3>{song.title}</h3>
+                <p>Artiist: {song.artist_names}</p>
+                <a href={song.url}>Lyrics</a>
+            </div>
+        ))
+    }
+    
+    return (
+        <div>
+            <Form>
+                <Form.Group className="mb-3" controlId="form.Song">
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Enter a song" 
+                        value={search} 
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </Form.Group>
+            </Form>
+            {content}
+        </div>
+    );
+}
+
+export default Search;
