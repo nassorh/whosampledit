55a6689b34b3f8eb5bae12eb1515de57565f9c9d
nassorh <nassorh.dev@gmail.com@ --global>
2023-09-28 19:48:52 +0100

    Refactor: List Item component props handling

--- src/Compontents/ListItem.tsx
+++ src/Compontents/ListItem.tsx
@@ -2,18 +2,22 @@ import './ListItem.scss'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 interface ListItemProps{
+    id: number
+    title: string
     image: string
+    artist_name : string
+    
 }
 
 export default function ListItem(props: ListItemProps){
     return(
         <div className="list-item">
-            <div className="list-item-container">
-                <img src={props.image}/>
+            <div key={props.id} className="list-item-container">
+                <img src={props.image} alt={props.title}/>
 
                 <div className="list-item-content">
-                    <p className='list-item-content-title'>Song Title</p>
-                    <p className='list-item-content-artist'>Song Artist</p>
+                    <p className='list-item-content-title'>{props.title}</p>
+                    <p className='list-item-content-artist'>{props.artist_name}</p>
                 </div>
 
                 <div className="play-wrapper">
