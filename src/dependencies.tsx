27a4ce75de3cac70bb8483a2f81ef43d2dc9cd4e
nassorh <nassorh.dev@gmail.com@ --global>
2023-09-28 19:38:59 +0100

    Refactor : Removed use cases to clean up architecture

--- src/dependencies.tsx
+++ src/dependencies.tsx
@@ -1,9 +1,7 @@
-import SearchService from "./UseCases/perfomSearch";
 import GenuisApiAdapter from "./Adapaters/GeniusAPIAdapter";
 import GenuisApi from "./ExternalServices/GenuisApi";
 
 const musicApi = new GenuisApi(process.env.REACT_APP_GENIUS_API_ACCESS_TOKEN)
 const apiAdpater = new GenuisApiAdapter(musicApi)
-const searchService = new SearchService(apiAdpater)
 
-export default searchService;
+export default apiAdpater;
