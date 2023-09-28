2183d970b1408b5b0612ccafb04636da29ce747d
nassorh <nassorh.dev@gmail.com@ --global>
2023-09-28 19:50:00 +0100

    Feature : Added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
-import Search from './Compontents/Search';
+import './App.scss';
+import Index from './Pages/Index'
+
+import Search from './Pages/Search';
+import Sample from './Pages/Sample';
+
+import { RouterProvider,createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+	{
+		path: '',
+		element: <Index />,
+		children: [
+			{path: '', element: <Search />},
+      		{path: 'sample/:songId', element: <Sample />}
+		]
+	}
+])
 
 function App() {
-  return (
-    <div className="App">
-      <Search />
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- src/Compontents/SearchCard.tsx
+++ src/Compontents/SearchCard.tsx
@@ -1,32 +1,36 @@
 import Song from '../Entities/Song'
 import './SearchCard.scss'
 
+import { Link } from 'react-router-dom';
+
 interface SearchCardProps {
     song: Song;
 }
 
 export default function SearchCard(props : SearchCardProps){
     return(
-        <div key={props.song.id} className='search_card'>
+        <Link to={`/sample/${props.song.id}`}>
+            <div key={props.song.id} className='search_card'>
 
-            <div className='search_card-image-container'>
-                <img src={props.song.image_url} alt={props.song.title}/>
+                <div className='search_card-image-container'>
+                    <img src={props.song.image_url} alt={props.song.title}/>
+                </div>
+
+                <div className='search_card-info-container'>
+                    <div className='search_card-title-container'>
+                        <p className='search_card-song-title'>{props.song.title}</p>
+                        <p className='search_card-artist-name'>{props.song.artist_names}</p>
+                    </div>
+
+                    <div className='search_card-full-title'>
+                        <p>{props.song.full_title}</p>
+                    </div>
+
+                    <div className='search_card-release-date'>
+                        <p>{props.song.release_date}</p>
+                    </div>
+                </div>
             </div>
-
-            <div className='search_card-info-container'>
-                <div className='search_card-title-container'>
-                    <p className='search_card-song-title'>{props.song.title}</p>
-                    <p className='search_card-artist-name'>{props.song.artist_names}</p>
-                </div>
-
-                <div className='search_card-full-title'>
-                    <p>{props.song.full_title}</p>
-                </div>
-
-                <div className='search_card-release-date'>
-                    <p>{props.song.release_date}</p>
-                </div>
-            </div>
-        </div>
+        </Link>
     )
 }
