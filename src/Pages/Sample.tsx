77bc5d081c7e5f6b1d4f4292fb4134a38b01f442
nassorh <nassorh.dev@gmail.com@ --global>
2023-09-29 18:03:00 +0100

    Feature : Added back button to sample page

--- src/Pages/Sample.tsx
+++ src/Pages/Sample.tsx
@@ -1,6 +1,9 @@
 //Externals
 import { useState,useEffect } from 'react';
 import { useParams } from 'react-router-dom'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { common } from '@mui/material/colors';
+import { Link  } from 'react-router-dom';
 
 //Business Logic
 import searchService from '../dependencies'
@@ -41,7 +44,9 @@ export default function Sample(){
 
     return(
         <div>
-
+            <Link to={'..'}>
+                <ArrowBackIosIcon fontSize="large" sx={{ color: common.white, marginLeft:'6.5rem', marginTop: '2rem', marginBottom:'2rem' }} />
+            </Link>
             {!isLoading &&  samples === null && <Message type='hinter' text={"Oops, it looks like we couldn't find any samples or data to display."} />}
             {!isLoading &&  samples !== null && <List data = {samples} />}
             {isLoading && <p className='message'>Fetching Samples</p>}
